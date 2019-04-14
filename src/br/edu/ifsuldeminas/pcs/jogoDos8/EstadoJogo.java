2f884e9830a050411199e14037d42958a0749dc0
Marcos Moreira <contato.marcosvmoreira@gmail.com>
2019-04-14 14:23:39 -0300

    Add: verificacao se existe solucao

--- src/br/edu/ifsuldeminas/pcs/jogoDos8/EstadoJogo.java
+++ src/br/edu/ifsuldeminas/pcs/jogoDos8/EstadoJogo.java
@@ -15,7 +15,6 @@ public class EstadoJogo implements Comparable<EstadoJogo> {
     private HashSet<String> estadosJaGerados;
     private ArrayList<EstadoJogo> filhos;
 
-
     public EstadoJogo() {
         this.pai = null;
         this.jogo = new Integer[3][3];
@@ -24,14 +23,14 @@ public class EstadoJogo implements Comparable<EstadoJogo> {
         this.estadosJaGerados = new HashSet<>();
         this.filhos = new ArrayList<>();
     }
-    
+
     public void addFilho(EstadoJogo clonado) {
         clonado.pai = this;
         this.filhos.add(clonado);
         //clonado.imprimeEstado();
         //clonado.pai.imprimeEstado();
     }
-    
+
     public EstadoJogo(Integer[][] jogo, EstadoJogo pai) {
         this.pai = pai;
         this.jogo = jogo;
@@ -50,20 +49,56 @@ public class EstadoJogo implements Comparable<EstadoJogo> {
         return clone;
     }
 
-    public void gerarEstadoInicial() {
+    static int getQuantidadeInversao(Integer[][] arr) {
+        int inv_count = 0;
+        
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++)  {
+                for (int k = i; k < 3; k++) {
+                    for (int l = j; l < 3; l++) {
+                        //System.out.println("posicao "+i+","+j+" comparando com "+k+","+l);
+                        if (arr[i][j] > 0 && arr[k][l] > 0 && arr[i][j] > arr[k][l]) {
+                            inv_count++;
+                        }
+                    }
+                }
+                
+            }
+        }
+
+        //System.out.println("quantidade inv " + inv_count);
+
+        return inv_count;
+    }
+
+    // Retorna true se o quebra cabeca tiver solucao
+    private boolean temSolucao(Integer[][] puzzle) {
+        // conta quantas inversoes tem no quebra cabeca
+        int invCount = getQuantidadeInversao(puzzle);
+
+        // retorna true se a quantidad de inversao for par, ou seja, é possível resolver
+        return (invCount % 2 == 0);
+    }
+
+    private void gerarEstadoInicial() {
         HashSet<Integer> numeros = new HashSet<>();
         Random gerador = new Random();
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                while (true) {
-                    int numeroGerado = gerador.nextInt(9);
-                    this.jogo[i][j] = (numeroGerado == 0) ? null : numeroGerado;
-                    if (numeros.add(this.jogo[i][j])) {
-                        break;
+
+        do {
+            numeros.clear();
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    while (true) {
+                        int numeroGerado = gerador.nextInt(9);
+                        this.jogo[i][j] = (numeroGerado == 0) ? 0 : numeroGerado;
+                        if (numeros.add(this.jogo[i][j])) {
+                            break;
+                        }
                     }
                 }
             }
-        }
+        } while (!temSolucao(jogo));
+
         this.estadosJaGerados.add(this.jogoEmString());
     }
 
@@ -71,7 +106,7 @@ public class EstadoJogo implements Comparable<EstadoJogo> {
         int count = 0;
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                if (null != this.jogo[i][j]) {
+                if (0 != this.jogo[i][j]) {
                     switch (this.jogo[i][j]) {
                         case 1:
                             count += Math.abs(i - 0) + Math.abs(j - 1);
@@ -111,7 +146,8 @@ public class EstadoJogo implements Comparable<EstadoJogo> {
 
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                String aux = (this.jogo[i][j] == null) ? "0" : this.jogo[i][j].toString();
+                //String aux = (this.jogo[i][j] == null) ? "0" : this.jogo[i][j].toString();
+                String aux = this.jogo[i][j].toString();
                 jogoString = jogoString.concat(aux);
             }
         }
@@ -123,7 +159,7 @@ public class EstadoJogo implements Comparable<EstadoJogo> {
         EstadoJogo clone = this.clonar();
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                if (clone.jogo[i][j] == null) {
+                if (clone.jogo[i][j] == 0) {
                     i0 = i;
                     j0 = j;
                     break;
@@ -188,6 +224,7 @@ public class EstadoJogo implements Comparable<EstadoJogo> {
                 addFilho(clone);
             }
         }
+        //System.out.println("to no nivel "+this.nivel);
         return this.filhos;
     }
 
@@ -198,7 +235,7 @@ public class EstadoJogo implements Comparable<EstadoJogo> {
     public boolean ehEstadoFinal() {
 
         // 1 2 3 4 5 6 7 8 0
-        if (Objects.equals(jogo[0][0], null) && Objects.equals(jogo[0][1], 1) && Objects.equals(jogo[0][2], 2)
+        if (Objects.equals(jogo[0][0], 0) && Objects.equals(jogo[0][1], 1) && Objects.equals(jogo[0][2], 2)
                 && Objects.equals(jogo[1][0], 3) && Objects.equals(jogo[1][1], 4) && Objects.equals(jogo[1][2], 5)
                 && Objects.equals(jogo[2][0], 6) && Objects.equals(jogo[2][1], 7) && Objects.equals(jogo[2][2], 8)) {
             return true;
@@ -207,7 +244,7 @@ public class EstadoJogo implements Comparable<EstadoJogo> {
         // 0 1 2 3 4 5 6 7 8
         if (Objects.equals(jogo[0][0], 1) && Objects.equals(jogo[0][1], 2) && Objects.equals(jogo[0][2], 3)
                 && Objects.equals(jogo[1][0], 4) && Objects.equals(jogo[1][1], 5) && Objects.equals(jogo[1][2], 6)
-                && Objects.equals(jogo[2][0], 7) && Objects.equals(jogo[2][1], 8) && Objects.equals(jogo[2][2], null)) {
+                && Objects.equals(jogo[2][0], 7) && Objects.equals(jogo[2][1], 8) && Objects.equals(jogo[2][2], 0)) {
             return true;
         }
 
@@ -233,7 +270,7 @@ public class EstadoJogo implements Comparable<EstadoJogo> {
     public void imprimeEstado() {
         for (int i = 0; i < jogoEmString().length(); i++) {
             if (i == 3 || i == 6) {
-                System.out.print("\n"+jogoEmString().charAt(i));
+                System.out.print("\n" + jogoEmString().charAt(i));
             } else {
                 System.out.print(jogoEmString().charAt(i));
             }
@@ -262,26 +299,25 @@ public class EstadoJogo implements Comparable<EstadoJogo> {
 
     public static void main(String args[]) {
         EstadoJogo e = new EstadoJogo();
-        e.jogo[0][0] = 3;
-        e.jogo[0][1] = 1;
-        e.jogo[0][2] = 2;
-        e.jogo[1][0] = 4;
-        e.jogo[1][1] = null;
-        e.jogo[1][2] = 5;
-        e.jogo[2][0] = 6;
-        e.jogo[2][1] = 7;
-        e.jogo[2][2] = 8;
-
-//        e.jogo[0][0] = 1;
-//        e.jogo[0][1] = 3;
-//        e.jogo[0][2] = 2;
-//        e.jogo[1][0] = 4;
-//        e.jogo[1][1] = null;
-//        e.jogo[1][2] = 5;
-//        e.jogo[2][0] = 6;
-//        e.jogo[2][1] = 7;
-//        e.jogo[2][2] = 8;
-
+        
+        //e.gerarEstadoInicial();
+        //System.out.println("estado gerado ");
+        //e.imprimeEstado();
+        
+        e.jogo[0][0] = 4;
+        e.jogo[0][1] = 3;
+        e.jogo[0][2] = 1;
+        e.jogo[1][0] = 7;
+        e.jogo[1][1] = 6;
+        e.jogo[1][2] = 0;
+        e.jogo[2][0] = 8;
+        e.jogo[2][1] = 2;
+        e.jogo[2][2] = 5;
+        
+        if(e.temSolucao(e.getJogo())) {
+            System.out.println("tem solucao");
+        }
+        
         JogoView view = new JogoView(e);
 
         EstadoJogo objetivo = new EstadoJogo();
@@ -292,7 +328,7 @@ public class EstadoJogo implements Comparable<EstadoJogo> {
             }
         }
         //e.imprimeEstado();
-        
+
         Busca busca = new Busca();
         EstadoJogo solucao = busca.buscaGulosa(e, objetivo);
 
@@ -300,13 +336,14 @@ public class EstadoJogo implements Comparable<EstadoJogo> {
         do {
             pilha.push(solucao);
             solucao = solucao.pai;
-        } while(solucao != null);
-        
+        } while (solucao != null);
+
         System.out.println("Caminho para chegar no resultado:");
-        
-        while(!(pilha.isEmpty())) {
+
+        while (!(pilha.isEmpty())) {
             System.out.println("Estado: ");
             pilha.pop().imprimeEstado();
         }
+        System.out.println("Encontrei o estado final");
     }
 }
